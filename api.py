17f3fec5cf173212ba0f55a3a544d75950419ac8
mixailo77-hikari <mixailo77@gmail.com>
2026-09-07 01:24:35 +0500

    Update api.py

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ app.add_middleware(
     allow_origins=[
         "https://coolray775agz05.netlify.app",
         "http://coolray775agz05.netlify.app",
-        "https://mixailo77-hikari.github.io/coolray775agz05/",
+        "https://mixailo77-hikari.github.io",
         "http://localhost:3000",
     ],
     allow_credentials=True,
